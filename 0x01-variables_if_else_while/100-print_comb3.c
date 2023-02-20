98ca5725f8112a622119812b63cc3ff28140d3be
OLIVE-WRITE <you@ifeanyioliver2018@gmail.com>
2023-02-20 01:24:31 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,32 +7,22 @@
  */
 int main(void)
 {
-    int i, j;
+        int i, j;
 
-    for (i = 0; i < 100; i++)
-    {
-        j = i + 1;
-
-        while (j < 100)
+        for (i = 0; i < 9; i++)
         {
-            putchar((i / 10) + '0');
-            putchar((i % 10) + '0');
-            putchar(' ');
-            putchar((j / 10) + '0');
-            putchar((j % 10) + '0');
-
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-
-            j++;
+                for (j = i + 1; j <= 9; j++)
+                {
+                        putchar(i + '0');
+                        putchar(j + '0');
+                        if (i < 8)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
         }
-    }
-
-    putchar('\n');
-
-    return (0);
+        putchar('\n');
+        return (0);
 }
 
